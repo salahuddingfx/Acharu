df5fc9a86a10a051f3e3b13588612a76d3c64cd6
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-13 15:09:08 +0600

    feat: add service worker implementation using Workbox for precaching and navigation routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pq3hkv25h1o"
+    "revision": "0.c2u50qm0844"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
