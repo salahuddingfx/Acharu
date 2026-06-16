0e0567038766b57c9498540f228537af01631869
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-16 19:09:01 +0600

    feat: add service worker configuration with Workbox precaching and routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.c2u50qm0844"
+    "revision": "0.3rjnaktgee"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
