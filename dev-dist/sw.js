26a89eeb7a0b535a2bf6d48f0e340e35aa5c5378
salahuddingfx <salauddinkaderappy@gmail.com>
2026-07-03 16:43:32 +0600

    fix: update service worker revision and adjust push notification prompt positioning

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h8k7m2u4tsc"
+    "revision": "0.21snoo9ee4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
